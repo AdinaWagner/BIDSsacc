c7ce5a968986ac366bf647a6d8bb0526bb3b0670
Adina Wagner <adina.wagner@t-online.de>
2018-11-06 16:08:19 -0500

    add recipe for singularity image

---
+++ generate_simg.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+#Generate singularity image
+
+sudo docker run --rm kaczmarj/neurodocker:master generate singularity \
+--base neurodebian:stretch-non-free \
+--pkg-manager apt \
+--install fsl-5.0-core fsl-mni152-templates python-mvpa2 python-nipype python-matplotlib python-sklearn ipython \
+--add-to-entrypoint "source /etc/fsl/5.0/fsl.sh" > singularity.1.0
