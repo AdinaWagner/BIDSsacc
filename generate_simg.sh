2fc7eca16820d2f7ffdcb1aec84d24fea1738d11
Adina Wagner <adina.wagner@t-online.de>
2018-11-30 12:35:30 -0500

    Adjusted neurodocker script to create /data and /backup directories
    I discovered that I cannot use this image on hydra,
    while under /data and bunch of directories which are symlinks
    to /backup, And hydra does not have capability for the overlays.
    So, to mitigate we precreate those directories for binding

--- generate_simg.sh
+++ generate_simg.sh
@@ -8,4 +8,6 @@ sudo docker run --rm kaczmarj/neurodocker:master generate singularity \
 --base neurodebian:stretch-non-free \
 --pkg-manager apt \
 --install fsl-5.0-core fsl-mni152-templates python-mvpa2 python-nipype python-matplotlib python-sklearn ipython \
---add-to-entrypoint "source /etc/fsl/5.0/fsl.sh" > Singularity.1.0
+--add-to-entrypoint "source /etc/fsl/5.0/fsl.sh" \
+--run-bash "mkdir -p /data /backup" \
+> envs/Singularity.1.1
