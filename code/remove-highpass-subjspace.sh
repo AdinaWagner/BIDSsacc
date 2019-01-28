078a9fce593f2ab6603600f852d2b51f7bec9b59
Adina Wagner <adina.wagner@t-online.de>
2019-01-28 15:32:43 -0500

    script to remove highpassed files in subject space, for now only 2 runs, edit later

---
+++ code/remove-highpass-subjspace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## We said "Fuck it all" and will remove everything that the model does not
+## directly need from the directory.
+## for now only two runs, later this needs to be {1 .. 8}
+
+set -eu
+
+git rm sub-*/*/*/*run-{1..2}_desc-highpass_bold*
